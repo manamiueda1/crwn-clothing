4f4498fce02501f3a6a932e400d7fec0410a8a68
Manami Ueda <manamiueda1@gmail.com>
2020-12-24 16:56:15 -0500

    added header

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import './App.css';
 
 import HomePage from '../src/pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component.jsx'
-
+import Header from './components/header/header.component.jsx'
 
 
 function App() {
   return (
     <div >
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
       </Switch>
 
-      {/* <HomePage/> */}
     </div>
   );
 }
